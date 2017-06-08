fa4aa2772e36e32910eb7e78377bf2847f612096
Łukasz Łoboda <ll@colortv.com>
2017-06-08 14:21:20 +0200

    Added Assets endpoint support

---
+++ test/testPublicAssets.js
@@ -0,0 +1,57 @@
+const should = require('chai').should
+const assert = require('chai').assert
+const KrakenAssets = require('../lib/index').Assets
+
+should()
+
+describe('Kraken Assets Endpoint', function() {
+
+  const krakenAssets = new KrakenAssets()
+
+  it('should get all assets', function() {
+    return krakenAssets.getAllAssets()
+      .then(assets => {
+        const assetsCount = Object.keys(assets).length
+        return assetsCount.should.be.greaterThan(2)
+      })
+  })
+
+  it('should get only get selected assets', function () {
+    const selectedAssets = ['XBT', 'ETH']
+    return krakenAssets.getAssets(selectedAssets)
+      .then(assets => {
+        const assetsCount = Object.keys(assets).length
+        assetsCount.should.be.equal(selectedAssets.length)
+      })
+  })
+
+  it('should get only one selected asset', function() {
+    const selectedAsset = 'XBT'
+    return krakenAssets.getAsset(selectedAsset)
+      .then(asset => {
+        const assetsCount = Object.keys(asset).length
+        assetsCount.should.be.equal(1)
+      })
+  })
+
+  it('should fail when non-string assets keys are used', function() {
+    const malformedAssetsArray = [1, {}, function() {}]
+    assert.throws(() => {
+      krakenAssets.getAssets(malformedAssetsArray)
+    })
+  })
+
+  it('should fail to get single asset when empty string or malformed input is used', function() {
+    assert.throws(() => {
+      const emptyAssetId = ''
+      krakenAssets.getAsset(emptyAssetId)
+    })
+
+    assert.throws(() => {
+      const malformedAssetId = {'malformed': 'input'}
+      krakenAssets.getAsset(malformedAssetId)
+    })
+
+  })
+
+})
